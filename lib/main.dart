d2024152cfa321d37fdff8fb7ae0041d850c5660
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-03-26 05:26:40 -0300

    add state to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(ToDoList());
+void main() => runApp(ToDoListApp());
 
-class ToDoList extends StatelessWidget {
+class ToDoListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Todo List',
-      home: new Scaffold(
-          appBar: new AppBar(
-              title: new Text('Todo List')
-          )
-      ),
+              title: 'Todo List',
+              home: new TodoList()
+          );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  createState() => new TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+            title: new Text('Todo List')
+        )
     );
   }
 }
